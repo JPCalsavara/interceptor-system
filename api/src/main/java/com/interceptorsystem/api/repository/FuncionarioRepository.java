33faa42f72aec47b9336d42c06af237808251968
JPCalsavara <jpcalsavara@gmail.com>
2025-08-10 14:56:15 -0300

    fix: arrumando a referencia

--- api/src/main/java/com/interceptorsystem/api/repository/FuncionarioRepository.java
+++ api/src/main/java/com/interceptorsystem/api/repository/FuncionarioRepository.java
@@ -2,7 +2,7 @@ package com.interceptorsystem.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.interceptorsystem.api.domain.entity.FuncionarioEntity;
+import com.interceptorsystem.api.entity.FuncionarioEntity;
 
 import java.util.UUID;
 
