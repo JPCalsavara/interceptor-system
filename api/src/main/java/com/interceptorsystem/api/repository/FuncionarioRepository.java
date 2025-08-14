331a15fe1bd05d09f35253a1dfb26af5ade24db1
JPCalsavara <jpcalsavara@gmail.com>
2025-08-14 11:49:59 -0300

    feat: adicionando repostorio do crud de Funcionario

--- api/src/main/java/com/interceptorsystem/api/repository/FuncionarioRepository.java
+++ api/src/main/java/com/interceptorsystem/api/repository/FuncionarioRepository.java
@@ -1,11 +1,16 @@
 package com.interceptorsystem.api.repository;
 
+import com.interceptorsystem.api.domain.vo.CPF;
+import com.interceptorsystem.api.entity.FuncionarioEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.interceptorsystem.api.entity.FuncionarioEntity;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface FuncionarioRepository extends JpaRepository<FuncionarioEntity, UUID> {
+    // O Spring Data JPA cria a query automaticamente com base no nome do método.
+    // Ele sabe como buscar dentro do Value Object CPF.
+    Optional<FuncionarioEntity> findByCpf(CPF cpf);
 }
